cacf0d2eae9aec0717d92b38c7d8014454b5a218
Isuru Perera <isuru.perera@sonarsource.com>
2025-08-29 15:06:18 +0800

    "Changes committed by sonar-qube-agent with ID - 100"

--- sorts/cyclic_sort.py
+++ sorts/cyclic_sort.py
@@ -32,10 +32,7 @@ def cyclic_sort(nums: list[int]) -> list[int]:
     # Perform cyclic sort
     index = 0
     while index < len(nums):
-        # Calculate the correct index for the current element
         correct_index = nums[index] - 1
-        # If the current element is not at its correct position,
-        # swap it with the element at its correct index
         if index != correct_index:
             nums[index], nums[correct_index] = nums[correct_index], nums[index]
         else:
