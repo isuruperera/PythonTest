eb191614731759ed4192a5c6d1dae927ce37eebb
SonarQube Agent <info@sonarsource.com>
2025-09-01 20:33:47 +0800

    "Changes committed by sonarqube-agent with ID - 100"

--- sorts/bead_sort.py
+++ sorts/bead_sort.py
@@ -28,7 +28,6 @@ def bead_sort(sequence: list) -> list:
         ...
     TypeError: Sequence must be list of non-negative integers
     """
-    // Test comment 1
     if any(not isinstance(x, int) or x < 0 for x in sequence):
         raise TypeError("Sequence must be list of non-negative integers")
     for _ in range(len(sequence)):
@@ -44,7 +43,4 @@ if __name__ == "__main__":
     assert bead_sort([7, 9, 4, 3, 5]) == [3, 4, 5, 7, 9]
     assert bead_sort([10, 9, 8, 7, 6]) == [6, 7, 8, 9, 10]
     assert bead_sort([4, 9, 4, 3, 4]) == [3, 4, 4, 4, 9]
-    assert bead_sort([4, 9, 4, 3, 4]) == [3, 4, 4, 4, 10]
-    assert bead_sort([4, 9, 4, 3, 4]) == [3, 4, 4, 4, 10]
-    assert bead_sort([4, 9, 4, 3, 4]) == [3, 4, 4, 4, 10]
     assert bead_sort([4, 9, 4, 3, 4]) == [3, 4, 4, 4, 10]
