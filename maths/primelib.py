e54781e044b3eeb931b16a4fe7a3e8dddbc4ff74
Agent Orchestrator <agent-orchestrator@sonarsource.com>
2026-07-15 13:45:49 +0000

    fix: SonarQube issue remediation [agent-orchestrator]

--- maths/primelib.py
+++ maths/primelib.py
@@ -41,6 +41,8 @@ from math import sqrt
 
 from maths.greatest_common_divisor import gcd_by_iterative
 
+NUMBER_VALIDATION_ERROR_MSG = "'number' must been an int and >= 0"
+
 
 def is_prime(number: int) -> bool:
     """
@@ -198,7 +200,7 @@ def prime_factorization(number):
     """
 
     # precondition
-    assert isinstance(number, int) and number >= 0, "'number' must been an int and >= 0"
+    assert isinstance(number, int) and number >= 0, NUMBER_VALIDATION_ERROR_MSG
 
     ans = []  # this list will be returns of the function.
 
@@ -254,9 +256,7 @@ def greatest_prime_factor(number):
     """
 
     # precondition
-    assert isinstance(number, int) and (number >= 0), (
-        "'number' must been an int and >= 0"
-    )
+    assert isinstance(number, int) and (number >= 0), NUMBER_VALIDATION_ERROR_MSG
 
     ans = 0
 
@@ -296,9 +296,7 @@ def smallest_prime_factor(number):
     """
 
     # precondition
-    assert isinstance(number, int) and (number >= 0), (
-        "'number' must been an int and >= 0"
-    )
+    assert isinstance(number, int) and (number >= 0), NUMBER_VALIDATION_ERROR_MSG
 
     ans = 0
 
